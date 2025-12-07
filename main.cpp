ec2152f38cd72842d57f7ce109023f9736e190d3
Colton Rizzo <rizzocolton@gmail.com>
2025-12-06 19:42:44 -0500

    Added controls window

--- main.cpp
+++ main.cpp
@@ -3,20 +3,45 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "PhysiSim");
+    //creating display window
+    sf::RenderWindow window(sf::VideoMode({1600, 1000}), "PhysiSim",sf::Style::Close);
+    sf::RenderWindow controls(sf::VideoMode({500,500}),"Controls",sf::Style::Titlebar);
+
+    //setup shapes
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+    while (window.isOpen()){
+
+        //checks all window events that were triggered since last loop
+        while (const std::optional event = window.pollEvent()){   
+            //if close is requested, then close the window
+            if (event->is<sf::Event::Closed>()){
                 window.close();
+                controls.close();
+            }
         }
 
+        while(const std::optional event = controls.pollEvent()){   
+            
+        }
+
+        //* PRIMARY WINDOW RENDERING */
+
+        //clear frame
         window.clear();
+        //draw on frame
+
+
         window.draw(shape);
+
+        //end frame
         window.display();
+
+        //* CONTROL WINDOW RENDERING */
+
+        controls.clear();
+        
+        controls.display();
     }
 }
